5c4902531be7e7bfa2fbc09387c9abde0a55b945
Maksym Kosov <faktoriya07@gmail.com>
2026-08-29 06:52:48 +0300

    fix: pass ALLURE_CONFIG env variable in workflow

--- TestsAPI/UserApiTests.cs
+++ TestsAPI/UserApiTests.cs
@@ -6,6 +6,9 @@ using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
+// Регистрируем Allure как кастомный репортер xUnit
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+
 namespace AqaPortfolioProject.TestsApi
 {
     [AllureEpic("API Testing")]
